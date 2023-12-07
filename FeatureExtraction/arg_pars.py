eef159074c986083c0697b63c65bc1f5240230cc
trsjulie <37087267+trsjulie@users.noreply.github.com>
2023-12-07 15:12:35 +0100

    Update arg_pars.py

--- FeatureExtraction/arg_pars.py
+++ FeatureExtraction/arg_pars.py
@@ -3,8 +3,8 @@ import argparse
 parser = argparse.ArgumentParser()
 
 
-parser.add_argument('--video_dir', default="/home/julie/ssd/data1/Clips_mp4_2/")
-parser.add_argument('--feature_dir', default="/home/julie/ssd/data1/Features/XCLIP/Visual")
+parser.add_argument('--video_dir', default="/home/xxx/ssd/data1/Clips_mp4_2/")
+parser.add_argument('--feature_dir', default="/home/xxx/ssd/data1/Features/XCLIP/Visual")
 parser.add_argument('--model_type', default="XCLIP")
 parser.add_argument('--model_name', default = "microsoft/xclip-base-patch16-zero-shot")
 
